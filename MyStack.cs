c81aacfc6bbf0cb25a7b6b8967222b8a94e76c91
Alexsander <alexdonay@gmail.com>
2024-04-06 18:32:53 -0300

    Adicionar arquivos de projeto.

---
+++ MyStack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class MyStack<T>
+    {
+        private int StackHead = -1;
+        private T[] Stack= new T[1];
+        public MyStack() { }
+        public void Push(T newItem)
+        {
+            StackHead++;
+            T[] newStack = new T[StackHead+1];
+            for(int i = 0; i < StackHead; i++)
+            {
+                newStack[i] = Stack[i];
+            }
+            newStack[StackHead] = newItem;
+            Stack = newStack;
+        }
+        public void Pop()
+        {
+            StackHead = StackHead - 1;
+        }
+        public T GetStackHead() {
+            return Stack[StackHead];
+        }
+        public bool IsEmpty()
+        {
+            return StackHead == 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "((1+2)*(4-2)/(2))";
+            char[] operators = new char[] { '+', '-', '*', '/' };
+            MyStack<float> numbersStack = new MyStack<float>();
+            MyStack<char> operatorsStack = new MyStack<char>();
+
+            foreach (char c in sentence)
+            {
+                if (Char.IsDigit(c))
+                {
+                    numbersStack.Push(c - '0');
+                }
+                else if (c == ')')
+                {
+                    float num2 = numbersStack.GetStackHead();
+                    numbersStack.Pop();
+                    float num1 = numbersStack.GetStackHead();
+                    numbersStack.Pop();
+                    char op = operatorsStack.GetStackHead();
+                    operatorsStack.Pop();
+
+                    float result = ApplyOperation(num1, num2, op);
+                    numbersStack.Push(result);
+                }
+                else if (Array.IndexOf(operators, c) != -1)
+                {
+                    operatorsStack.Push(c);
+                }
+            }
+
+            // O resultado final estará no topo da pilha de números
+            float finalResult = numbersStack.GetStackHead();
+            Console.WriteLine($"Resultado: {finalResult}");
+        }
+
+        static float ApplyOperation(float num1, float num2, char op)
+        {
+            switch (op)
+            {
+                case '+':
+                    return num1 + num2;
+                case '-':
+                    return num1 - num2;
+                case '*':
+                    return num1 * num2;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        return num1 / num2;
+                    }
+                    else
+                    {
+                        throw new DivideByZeroException("Divisão por zero!");
+                    }
+                default:
+                    throw new ArgumentException("Operador inválido!");
+            }
+        }
+    }
+}
